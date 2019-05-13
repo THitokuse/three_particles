286cb94048ace1ffe5023495988dd46565f0ab98
THitokuse <hasegawa.taichiro@gmail.com>
2019-05-13 23:21:47 +0900

    ヘルパーを設定

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ let box; //mesh
 let light;
 let ambient;
 let camera;
+let gridHelper;
+let axisHelper;
+let lightHelper;
 let renderer;
 let width = 500;
 let height = 250;
@@ -14,12 +17,12 @@ function init() {
   // sceneステージ
   scene = new THREE.Scene();
 
-  //mesh 物体
+  // mesh 物体
   // - geometry 形状
   // - material 材質
   box = new THREE.Mesh(
     new THREE.BoxGeometry(50, 50, 50),
-    new THREE.MeshLambertMaterial({ color: 0xff0000})
+    new THREE.MeshLambertMaterial({ color: 0xff0000}) //濃い赤色
   );
   box.position.set(0, 0, 0);
   scene.add(box);
@@ -29,13 +32,21 @@ function init() {
   light.position.set(0, 100, 30);
   scene.add(light);
 
-  ambient = new THREE.AmbientLight(0x404040);
+  ambient = new THREE.AmbientLight(0x404040); // approx Eclipse(黒系)
   scene.add(ambient);
 
-  //camera
+  // camera
   camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
   camera.position.set(200, 100, 300);
   camera.lookAt(scene.position);
+
+  // helper
+  gridHelper = new THREE.GridHelper(200,50);
+  scene.add(gridHelper);
+  axisHelper = new THREE.AxisHelper(1000);
+  scene.add(axisHelper);
+  lightHelper = new THREE.DirectionalLightHelper(light, 20);
+  scene.add(lightHelper);
 
   // renderer
   renderer = new THREE.WebGLRenderer({ antialias: true });
